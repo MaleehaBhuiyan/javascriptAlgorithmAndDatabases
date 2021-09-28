71aba6d2139d283eb7ab53498ef6b60b38e869ef
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-09-28 15:43:02 -0400

    Recursion someRecursive complete

---
+++ algorithms/Recursion/someRecursive.js
@@ -0,0 +1,7 @@
+const isOdd = val => val % 2 !== 0;
+
+function someRecursive(arr, callback){
+	if(arr.length === 0) return false; 
+	if(callback(arr[0]) === true) return true; 
+	return someRecursive(arr.slice(1), callback)
+}
